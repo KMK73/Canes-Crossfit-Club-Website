4c5dc3f8422b1b740f431952b6d9ca67237ed984
KMK73 <kmk73@miami.edu>
2015-04-09 11:52:52 -0400

    fixed iOS register to include athlete/coach sign up

--- api/register_ios.php
+++ api/register_ios.php
@@ -1,6 +1,8 @@
 <?php
 include 'connect.php';
 include 'MySQLDatabaseObject.php';
+//connect to database in mysql
+$sql_link = new mysqli("localhost", "peak_360", "admin", "peak_360") or die(mysqli_error());
 
 $first = $_POST['first_name'];
 $last = $_POST['last_name'];
@@ -8,9 +10,9 @@ $username = $_POST['username'];
 $password = $_POST['password'];
 $password = md5($passwod); //secure password hashing
 $confirm_password = $_POST['c_password'];
-$user_type = "Athlete";
+$user_type = $_POST['user_type'];
 
-$query = sprintf("INSERT INTO users (first_name, last_name, username, password, user_type) VALUES ('%s','%s','%s','%s')", $first, $last, $username, $password, $user_type);
+$query = sprintf("INSERT INTO users (first_name, last_name, username, password, user_type) VALUES ('%s','%s','%s','%s','%s')", $first, $last, $username, $password, $user_type);
 
 $result = mysqli_query($sql_link, $query);
 
