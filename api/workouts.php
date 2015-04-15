20128fe0fece1275b3d9bbe15ec42a3b8c37422d
KMK73 <kmk73@miami.edu>
2015-04-14 23:19:35 -0400

    fixed description bug in workouts api

--- api/workouts.php
+++ api/workouts.php
@@ -13,8 +13,10 @@
                 "Workout id" => $row['workout_id'],
                 "Workout Name" => $row['workout_name'], 
                 "WOD Type" => $row['wod_type'], 
-                "description" => $row['rep_description'],
-                "Date" => $row['wod_date'] );
+                //description field returning null
+                "Date" => $row['wod_date'],
+            "Description" => utf8_encode($row['description']));
+            
             $workouts[] = $workout_data;
         }
 		echo json_encode($workouts);
