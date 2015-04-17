34c84924c4eb2b52a86419f8e92364f44d20ffbe
KMK73 <kmk73@miami.edu>
2015-04-17 13:47:33 -0400

    workout api works

--- api/workouts.php
+++ api/workouts.php
@@ -4,25 +4,20 @@ ini_set('display_errors',1);
 ?>
    <?php
 		include 'connect.php';
-
 		$query = "SELECT * FROM workouts";
 
 		$result = mysqli_query($sql_link, $query);        
-		
         $workouts = array();
 
 		while ($row = mysqli_fetch_assoc($result)) {
-
-
- 			$workouts[] = array(
+            $workouts[] = array(
                 "id" => $row['workout_id'],
-                "Name" => $row['workout_name'], 
+                "name" => utf8_encode($row['workout_name']), 
                 "Type" => $row['wod_type'], 
-                //description field returning null
                 "Date" => $row['wod_date'],
-            "Description" => utf8_encode($row['description']));
-            
+            "Description" => utf8_encode($row['description'])
+            );
         }
-
-		echo json_encode($workouts);
+        $json = json_encode($workouts);
+        echo $json;
 ?>
